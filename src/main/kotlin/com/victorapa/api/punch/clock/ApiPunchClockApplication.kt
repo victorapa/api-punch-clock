6a7c0c6c43871f52c6fc7159ae5a837eca951fb4
victorapa <victorandradeap@gmail.com>
2020-05-10 23:55:19 -0300

    Create project

---
+++ src/main/kotlin/com/victorapa/api/punch/clock/ApiPunchClockApplication.kt
@@ -0,0 +1,11 @@
+package com.victorapa.api.punch.clock
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ApiPunchClockApplication
+
+fun main(args: Array<String>) {
+	runApplication<ApiPunchClockApplication>(*args)
+}
